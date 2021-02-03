2e81a351fcf5a507003b4273c4048b244cc8a7a3
Cooper Browning <Cooper.S.Browning-1@ou.edu>
2021-02-02 18:33:36 -0600

    Completed the example over creating variables, getting input, concatenation.

--- In_Class_Examples/VariablesInputConversionFromatting_Example/Program.cs
+++ In_Class_Examples/VariablesInputConversionFromatting_Example/Program.cs
@@ -6,7 +6,29 @@ namespace VariablesInputConversionFromatting_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; //idk why but we want to bump everyones gpa by .5
+
+            string output = "Hello " + firstName + " " + lastName + "! " + " we boosted your gpa to " + gpa.ToString("N3");
+           
+            //output = $"Hello {firstName} {lastName}! we boosted your gpa to {gpa.ToString("N3")}";
+            
+            Console.WriteLine(output);
+
+            
+
         }
     }
 }
